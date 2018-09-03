1c79bec9342c9a84675c628e0e7355198702d83d
Tamino Martinius <dev@zaku.eu>
2018-09-03 19:48:37 +0200

    Update insert and delete method names

--- src/Connector.ts
+++ src/Connector.ts
@@ -5,8 +5,8 @@ export abstract class Connector {
   public abstract async createTable(): Promise<void>;
   public abstract async dropTable(): Promise<void>;
   public abstract async getMigrationKeys(): Promise<string[]>;
-  public abstract async insertMigration(key: string): Promise<void>;
-  public abstract async deleteMigrations(key: string): Promise<void>;
+  public abstract async insertMigrationKey(key: string): Promise<void>;
+  public abstract async deleteMigrationKey(key: string): Promise<void>;
 }
 
 export default Connector;
