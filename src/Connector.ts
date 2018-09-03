785c82b722693600d32274f3f95aca209f63993c
Tamino Martinius <dev@zaku.eu>
2018-09-03 19:56:00 +0200

    Add new abstract methods for transactions

--- src/Connector.ts
+++ src/Connector.ts
@@ -7,6 +7,9 @@ export abstract class Connector {
   public abstract async getMigrationKeys(): Promise<string[]>;
   public abstract async insertMigrationKey(key: string): Promise<void>;
   public abstract async deleteMigrationKey(key: string): Promise<void>;
+  public abstract async beginTransaction(): Promise<void>;
+  public abstract async endTransaction(): Promise<void>;
+  public abstract async rollbackTransaction(): Promise<void>;
 }
 
 export default Connector;
